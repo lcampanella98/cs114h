3cf40ffc76904454815f517685b214dd9975e5d0
Lorenzo Campanella <enzocampanella98@gmail.com>
2017-11-20 21:57:49 -0500

    implemented heap sort for project 3

---
+++ src/projects/sorts/Sorts.java
@@ -0,0 +1,207 @@
+package projects.sorts;
+
+import java.util.Arrays;
+
+public class Sorts {
+
+    public static <T extends Comparable<? super T>> void heapSort(T[] array) {
+        // initial max heap
+        if (array.length == 0) return;
+        int i = (array.length - 1) / 2;
+        while (i >= 0) {
+            moveDownHeap(array, i--, array.length);
+        }
+        i = array.length - 1;
+        T temp;
+        while (i > 0) {
+            // swap top of heap with bottom
+            temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            // re-heapify
+            moveDownHeap(array, 0, i--);
+        }
+    }
+
+    private static <T extends Comparable<? super T>> void moveDownHeap(T[] array, int i, int lim) {
+        int li, ri, maxi;
+        T l, r, max, temp = array[i];
+        li = 2 * i + 1;
+        while (li < lim) {
+            ri = li + 1;
+            if (ri < lim) {
+                l = array[li];
+                r = array[ri];
+                if (l.compareTo(r) > 0) {
+                    max = l;
+                    maxi = li;
+                } else {
+                    max = r;
+                    maxi = ri;
+                }
+            } else {
+                max = array[li];
+                maxi = li;
+            }
+            if (temp.compareTo(max) < 0) {
+                array[i] = max;
+                array[maxi] = temp;
+                i = maxi;
+                li = 2 * i + 1;
+            } else break;
+        }
+    }
+
+    public static <T extends Comparable<? super T>> void bubbleSort(T[] array) {
+
+        T temp;
+        boolean sorted;
+
+        for (int i = array.length - 1; i > 0; --i) {
+
+            sorted = true;
+
+            for (int j = 0; j < i; ++j) {
+
+                if (array[j].compareTo(array[j + 1]) > 0) {
+
+                    sorted = false;
+
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+
+            if (sorted) {
+                break;
+            }
+        }
+    }
+
+    public static <T extends Comparable<? super T>> void insertSort(T[] array) {
+
+        T temp;
+        int j;
+
+        for (int i = 1; i < array.length; ++i) {
+
+            temp = array[i];
+
+            for (j = i; j > 0; --j) {
+
+                if (array[j - 1].compareTo(temp) > 0) {
+                    array[j] = array[j - 1];
+                }
+                else {
+                    break;
+                }
+            }
+
+            if (j < i) {
+                array[j] = temp;
+            }
+        }
+    }
+
+    public static <T extends Comparable<? super T>> void mergeSort(T[] array) {
+
+        if (array.length > 1) {
+
+            T[] left  = Arrays.copyOfRange(array, 0, array.length / 2);
+            T[] right = Arrays.copyOfRange(array, array.length / 2, array.length);
+
+            mergeSort(left);
+            mergeSort(right);
+
+            int i, l = 0, r = 0;
+
+            for (i = 0; i < array.length && l < left.length && r < right.length; ++i) {
+                if (left[l].compareTo(right[r]) <= 0) {
+                    array[i] = left[l++];
+                }
+                else {
+                    array[i] = right[r++];
+                }
+            }
+
+            if (i < array.length) {
+                if (l < left.length) {
+                    while (i < array.length) {
+                        array[i++] = left[l++];
+                    }
+                }
+                else {
+                    while (i < array.length) {
+                        array[i++] = right[r++];
+                    }
+                }
+            }
+        }
+    }
+
+    public static <T extends Comparable<? super T>> void quickSort(T[] array) {
+
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static <T extends Comparable<? super T>> void quickSort(T[] array, int left, int right) {
+
+        int pivot = right;
+
+        int l = left, r = right;
+
+        if (left < right) {
+            while (l < r) {
+
+                while (l < r && array[l].compareTo(array[pivot]) <= 0) {
+                    ++l;
+                }
+
+                while (l < r && array[pivot].compareTo(array[r]) <= 0) {
+                    --r;
+                }
+
+                if (l < r) {
+                    T temp = array[l];
+                    array[l] = array[r];
+                    array[r] = temp;
+                }
+            }
+
+            if (r != pivot) {
+                T temp = array[pivot];
+                array[pivot] = array[r];
+                array[r] = temp;
+            }
+
+            quickSort(array, left, r - 1);
+            quickSort(array, r + 1, right);
+        }
+    }
+
+    public static <T extends Comparable<? super T>> void selectSort(T[] array) {
+
+        T temp;
+        int mini;
+
+        for (int i = 0; i < array.length - 1; ++i) {
+
+            mini = i;
+
+            for (int j = i + 1; j < array.length; ++j) {
+
+                if (array[j].compareTo(array[mini]) < 0) {
+                    mini = j;
+                }
+            }
+
+            if (i != mini) {
+
+                temp = array[i];
+                array[i] = array[mini];
+                array[mini] = temp;
+            }
+        }
+    }
+}
